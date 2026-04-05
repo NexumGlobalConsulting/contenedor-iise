7d127808c03c7c7652f7675bdb6839b2d855f4cc
José Iván Tuesta Estrella <ivantuesta@gmail.com>
2026-04-05 11:37:17 -0500

    feat: carga optimizada del simulador NEXUM

--- .next/dev/server/chunks/ssr/_next-internal_server_app_simulador_page_actions_0.u7yjb.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/simulador/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_simulador_page_actions_0.u7yjb.js.map
--- .next/dev/static/chunks/app_simulador_page_tsx_0qc_95v._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_0p5v89f._.js","static/chunks/_0iymmfw._.js"],
-    source: "dynamic"
-});
